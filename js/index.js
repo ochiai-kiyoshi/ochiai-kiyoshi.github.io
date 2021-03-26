13da65ea995f251aa06cc68a6c2ffd4dcab8bd13
ochiaikiyoshi <4649kiyoshiochiai@gmail.com>
2021-03-26 18:04:12 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,218 @@
+$(window).on('load scroll', function (){
+
+    var box = $('.fadeIn');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn2');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn3');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn4');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn5');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn6');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn7');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn8');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn9');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn10');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn11');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadeIn12');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+  
+
+  
+  
